770dd026a39846727294f1fea32e918575c05e68
RodneyGG <rodneyarevalo9963@gmail.com>
2025-05-17 20:53:30 +0800

    class for users

--- quiz_program_logic.py
+++ quiz_program_logic.py
@@ -1,4 +1,10 @@
 #make a class for the users
+class Users:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        
 #Make a class for picking of the file
 #make a class for Questions generation
 #make a class for sending email
